a2a698b474bef96b13383fd64fa04f9fe125a460
Kennice Offor <kennice_offor@yahoo.com>
2020-04-20 20:02:20 +0800

    Configured Tags, Tagging and Show index to display Tags associated with each Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts do
-  resources :comments
-end
+
   root to: "posts#index"
+  resources :posts do
+    resources :comments
+  end
+  resources :tags
 end
